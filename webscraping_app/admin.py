3ad582f3dd6c169211a6e2620a1eb612f4a00866
jereme j <jeremedon123>
2020-10-12 11:49:43 +0530

    first commit

---
+++ webscraping_app/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from .models import search
+admin.site.register(search)
---
+++ webscraping_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WebscrapingAppConfig(AppConfig):
+    name = 'webscraping_app'
